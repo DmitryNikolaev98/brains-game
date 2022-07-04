e03f6f9157e558cd4309634f2f708a34e6520bfb
Dmitry Nikolaev <71396957+DmitryNikolaev98@users.noreply.github.com>
2022-07-04 10:17:20 +0300

    Update App.java

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -11,24 +11,14 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        static final int gameNumberCli = 1;
-        static final int gameNumberCalc = 2;
-        static final int gameNumberEven = 3;
-        static final int gameNumberGcd = 4;
-        static final int gameNumberPrime = 5;
-        static final int gameNumberProgression = 6;
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("""
-                Please enter the game number and press Enter.
-                1 - Greet\s
-                2 - Even\s
-                3 - Calc\s
-                4 - GCD\s
-                5 - Progression\s
-                6 - Prime\s
-                0 - Exit\s
-                Your choice:\s """);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
+        System.out.println("Your choice:");
 
         int number = scanner.nextInt();
 

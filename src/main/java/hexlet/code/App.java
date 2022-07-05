6835565a8c236b7583f455fa824ef858fe08d27f
Dmitry Nikolaev <71396957+DmitryNikolaev98@users.noreply.github.com>
2022-07-05 11:07:40 +0300

    change variables to strings

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -11,12 +11,6 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        final int greet = 1;
-        final int even = 2;
-        final int calc = 3;
-        final int gcd = 4;
-        final int progression = 5;
-        final int prime = 6;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
@@ -25,15 +19,16 @@ public class App {
         System.out.println("4 - GCD");
         System.out.println("5 - Progression");
         System.out.println("6 - Prime");
-        System.out.println("Your choice:");
-        int number = scanner.nextInt();
+        System.out.println("Your choice:")
+        String number = scanner.next();
+        
         switch (number) {
-            case greet -> Cli.greeting();
-            case even -> Even.evenGame();
-            case calc -> Calc.calcGame();
-            case gcd -> Gcd.gcdGame();
-            case progression -> Progression.progressionGame();
-            case prime -> Prime.primeGame();
+            case "1" -> Cli.greeting();
+            case "2" -> Even.evenGame();
+            case "3" -> Calc.calcGame();
+            case "4" -> Gcd.gcdGame();
+            case "5" -> Progression.progressionGame();
+            case "6" -> Prime.primeGame();
             default -> System.exit(0);
         }
     }

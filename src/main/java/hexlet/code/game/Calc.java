75c18d81abacf75f2011f9515303fde5beb56bab
Dmitry Nikolaev <71396957+DmitryNikolaev98@users.noreply.github.com>
2022-07-12 11:20:01 +0300

    created the Calculate method

--- src/main/java/hexlet/code/game/Calc.java
+++ src/main/java/hexlet/code/game/Calc.java
@@ -8,43 +8,43 @@ import java.util.Random;
 public class Calc {
 
     public static final int MAX_RANDOM_NUMBER = 15;
+    public static String[][] askAndQuestion = new String[Engine.NUMBER_ROUNDS][Engine.AMOUNT_QUESTION];
 
     public static void calcGame() {
-
+        
         String description = "Welcome to the Brain Games!";
-
-        String[][] askAndQuestion = new String[Engine.NUMBER_ROUNDS][Engine.AMOUNT_QUESTION];
-
+        
+        Calc.calculate();
+        
+        Engine.startGame(description, askAndQuestion);
+        
+    }
+    public static void calculate() {
+        
         for (int i = 0; i < Engine.NUMBER_ROUNDS; i++) {
-            int expression1 = Utils.generateRandomNumber(MAX_RANDOM_NUMBER);
-            int expression2 = Utils.generateRandomNumber(MAX_RANDOM_NUMBER);
+            int randomNumberOne = Utils.generateRandomNumber(MAX_RANDOM_NUMBER);
+            int randomNumberTwo = Utils.generateRandomNumber(MAX_RANDOM_NUMBER);
 
             String operators = String.valueOf(Calc.generateOperator());
 
             switch (operators) {
                 case "+" -> {
-                    askAndQuestion[i][Engine.QUESTION_NUMBER] = expression1 + " + " + expression2;
-                    askAndQuestion[i][Engine.ANSWER_NUMBER] = String.valueOf(expression1 + expression2);
+                    askAndQuestion[i][Engine.QUESTION_NUMBER] = randomNumberOne + " + " + randomNumberTwo;
+                    askAndQuestion[i][Engine.ANSWER_NUMBER] = String.valueOf(randomNumberOne + randomNumberTwo);
                 }
                 case "-" -> {
-                    askAndQuestion[i][Engine.QUESTION_NUMBER] = expression1 + " - " + expression2;
-                    askAndQuestion[i][Engine.ANSWER_NUMBER] = String.valueOf(expression1 - expression2);
+                    askAndQuestion[i][Engine.QUESTION_NUMBER] = randomNumberOne + " - " + randomNumberTwo;
+                    askAndQuestion[i][Engine.ANSWER_NUMBER] = String.valueOf(randomNumberOne - randomNumberTwo);
                 }
                 case "*" -> {
-                    askAndQuestion[i][Engine.QUESTION_NUMBER] = expression1 + " * " + expression2;
-                    askAndQuestion[i][Engine.ANSWER_NUMBER] = String.valueOf(expression1 * expression2);
+                    askAndQuestion[i][Engine.QUESTION_NUMBER] = randomNumberOne + " * " + randomNumberTwo;
+                    askAndQuestion[i][Engine.ANSWER_NUMBER] = String.valueOf(randomNumberOne * randomNumberTwo);
                 }
                 default -> {
 
                 }
             }
         }
-
-        Engine.startGame(description, askAndQuestion);
-
-    }
-    public static  void  calculate() {
-
     }
 
     public static String generateOperator() {

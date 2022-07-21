7150bcb3fad4987752e2da02eeb0dd4ef1979a8e
Dmitry Nikolaev <nik.dima001@yandex.ru>
2022-07-21 21:19:33 +0300

    Revert "update Progression"
    This reverts commit 5ff3e8baa78a79a3b20540f1b122a365d97f2c48.

--- src/main/java/hexlet/code/game/Progression.java
+++ src/main/java/hexlet/code/game/Progression.java
@@ -14,19 +14,34 @@ public class Progression {
         for (int i = 0; i < Engine.NUMBER_ROUNDS; i++) {
             int startProgressionGame = Utils.generateRandomNumber(MAX_RANDOM_NUMBER);
             int stepProgressionGame = Utils.differanceMaxAndMin(MAX_STEP, MIN_STEP);
+            int hiddenNumber = Utils.generateRandomNumber(LENGTH_PROGRESSION - 1);
             String[] rowNumberProgression = new String[LENGTH_PROGRESSION];
             String[] rowProgression = {"", "", ""};
-            int hiddenNumber = Utils.generateRandomNumber(LENGTH_PROGRESSION - 1);
-            String[] numbers = new String[LENGTH_PROGRESSION];
+            String[] numbers = generateProgression(startProgressionGame, stepProgressionGame);
 
             for (int j = 0; j < LENGTH_PROGRESSION; j++) {
-                numbers[j] = String.valueOf(startProgressionGame + (j * stepProgressionGame));
-                rowNumberProgression[j] = (j == hiddenNumber) ? ".. " : ((numbers[j]) + " ");
+                var result = new StringBuilder();
+
+                if (j == hiddenNumber) {
+                    result.append(".. ");
+                } else {
+                    result.append(numbers[j]).append(" ");
+                }
+                rowNumberProgression[j] = String.valueOf(result);
                 rowProgression[i] += rowNumberProgression[j];
-                askAndQuestion[i][Engine.ANSWER_NUMBER] = String.valueOf(numbers[hiddenNumber]);
-                askAndQuestion[i][Engine.QUESTION_NUMBER] = rowProgression[i];
             }
+
+            askAndQuestion[i][Engine.ANSWER_NUMBER] = String.valueOf(numbers[hiddenNumber]);
+            askAndQuestion[i][Engine.QUESTION_NUMBER] = rowProgression[i];
         }
         Engine.startGame(DESCRIPTION, askAndQuestion);
+    }
+    public static String[] generateProgression(int startProgressionGame, int stepProgressionGame) {
+        String[] progression = new String[LENGTH_PROGRESSION];
+
+        for (int j = 0; j < LENGTH_PROGRESSION; j++) {
+            progression[j] = String.valueOf(startProgressionGame + (j * stepProgressionGame));
+        }
+        return progression;
     }
 }
